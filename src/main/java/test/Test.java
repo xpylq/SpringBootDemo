ef1da92928bfc2494e9fc6d8c67aa78006765aff
hzyouzhihao <hzyouzhihao@corp.netease.com>
2016-04-26 21:17:36 +0800

    提交

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,42 +1,71 @@
 package test;
 
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by hzyouzhihao on 2016/4/26.
  */
 public class Test {
+    static class Ip {
+        int num;
+        String ip;
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public int getNum() {
+            return num;
+        }
+
+        public void setNum(int num) {
+            this.num = num;
+        }
+    }
+
+    static class IpComparator implements Comparator<String> {
+        public int compare(String o1, String o2) {
+            int flag = 0;
+            String[] o1Strs = o1.split("\\.");
+            String[] o2Strs = o2.split("\\.");
+            //如果不符合规范，就默认最小排序
+            if (o1Strs.length != 4 || o2Strs.length != 4) {
+                return -1;
+            }
+            for (int i = 0; i < o1Strs.length; i++) {
+                flag = Long.valueOf(o1Strs[i]).compareTo(Long.valueOf(o2Strs[i]));
+                if (flag != 0) {
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+
 
     public static void main(String[] args) {
-        class Ip {
-            int num;
-            String ip;
+        String inputFilePath = "e:\\input.txt";
+        String outputFilePath = "e:\\output.txt";
 
-            public String getIp() {
-                return ip;
-            }
+        processData(inputFilePath, outputFilePath);
+    }
 
-            public void setIp(String ip) {
-                this.ip = ip;
-            }
+    public static void processData(String inputFilePath, String outputFilePath) {
 
-            public int getNum() {
-                return num;
-            }
-
-            public void setNum(int num) {
-                this.num = num;
-            }
-        }
-        List<String> list = new ArrayList<String>();
-        list.add("1.12.193.198");
-        list.add("1.12.193.198");
-        list.add("1.12.106.136");
-        list.add("1.12.193.198");
-        list.add("1.12.106.136");
-        Collections.sort(list);
+        List<String> list = inputDataByFile(inputFilePath);
+        Collections.sort(list, new IpComparator());
         String temp = "";
         int num = 0;
         List<Ip> result = new ArrayList<Ip>();
@@ -50,10 +79,34 @@ public class Test {
             ip.setNum(num);
             result.add(ip);
         }
-        for (Ip ip : result) {
-            System.out.println(ip.getNum() + "-------" + ip.getIp());
-        }
+        outputData2File(result, outputFilePath);
     }
 
+    public static List<String> inputDataByFile(String inputFilePath) {
+        List<String> datas = null;
+        try {
+            Path path = FileSystems.getDefault().getPath(inputFilePath);
+            datas = Files.readAllLines(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return datas;
+    }
 
+    public static void outputData2File(List<Ip> list, String outputFilePath) {
+        try {
+            File file = new File(outputFilePath);
+            file.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for (Ip ip : list) {
+                writer.write(ip.getNum() + "--------" + ip.getIp());
+                writer.newLine();
+            }
+            writer.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/yzh/spring/boot/config/MvcConfig.java
+++ src/main/java/yzh/spring/boot/config/MvcConfig.java
@@ -4,12 +4,18 @@ import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by youzhihao on 16/4/18.
@@ -23,13 +29,13 @@ public class MvcConfig {
      * 自定义转换器,这里在保持默认的http转化器的基础上增加一个object对json转化器,默认的配置是存在这个
      * 转化器的,如果需要自己定制的话，需要自己制定一系列的converters
      */
-    @Bean
-    public HttpMessageConverters customConverters() {
-        HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
-        HttpMessageConverters converters = new HttpMessageConverters(jsonConverter, stringHttpMessageConverter);
-        return converters;
-    }
+//    @Bean
+//    public HttpMessageConverters customConverters() {
+//        HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+//        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+//        HttpMessageConverters converters = new HttpMessageConverters(jsonConverter, stringHttpMessageConverter);
+//        return converters;
+//    }
 
     /**
      * 这个指定请求的编码方式的filter在默认配置里是存在的，详见HttpEncodingAutoConfiguration类，如果自己定制web的话需要自己加上
@@ -43,5 +49,30 @@ public class MvcConfig {
         registrationBean.setFilter(filter);
 
         return filter;
+    }
+
+    /***
+     * 这里注入一个WebMvcConfigurer接口的包装类WebMvcConfigurerAdapter，可以自己定制自己的web配置，上面的httpMessageConvertor
+     * 可以在这里一并定制了。DelegatingWebMvcConfiguration复合类中的WebMvcConfigurerComposite里类中存在的一个集合WebMvcConfigurer，
+     * 自动注入所有装配的WebMvcConfigurer，并且叠加这些WebMvcConfigurer配置，而不是覆盖，顾如果你使用@EnableAutoConfiguration注解
+     * 会默认装配一个WebMvcConfigurer类，这里面包含一系列默认的组件，自己再增加一个则是叠加效果，如果想覆盖，则使用@EnableWebMvc配置
+     * 则不会生成默认的WebMvcConfigurer
+     */
+    @Bean
+    public WebMvcConfigurer webMvcConfigurerAdapter() {
+        WebMvcConfigurerAdapter webMvcConfigurerAdapter = new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+                converters.add(new MappingJackson2HttpMessageConverter());
+            }
+
+            /**增加一个静态文件映射，感觉没人什么用，用默认的/**即可  */
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/public1/**");
+            }
+        };
+        return webMvcConfigurerAdapter;
     }
 }
